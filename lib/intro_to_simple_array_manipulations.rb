f51ef186dc478337ab84dc5abfbf935ed5671ed5
michael james <majames071@gmail.com>
2020-09-13 19:51:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,8 +10,12 @@ def using_pop(array)
   array.pop()
 end
 
-def pop_with_arg(array)
+def pop_with_args(array)
   array.pop(2)
+end
+
+def using_shift(array)
+  array.shift()
 end
 
 def using_shift(array)
